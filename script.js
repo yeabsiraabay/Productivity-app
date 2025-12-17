34a81d0788cb8e93b455500fcaf19c4e4aa250cf
Yeabsira <amilayamayar1216@gmail.com>
2025-12-17 22:39:03 +0300

    Add DOM element references for task management
    Introduced DOM element references to support task management features, enabling easier manipulation and interaction with tasks in the UI.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// === Simple Productivity App ===
+
+// DOM elements
+const textInput = document.getElementById("taskText");
+const deadlineInput = document.getElementById("taskDeadline");
+const priority = document.getElementById("priority");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+const searchBox = document.getElementById("searchBox");
+const filter = document.getElementById("filter");
+const editTask = document.querySelector('.edit-task');
+const newName = document.querySelector('.newName');
+const newDate = document.querySelector('.newDate');
+const editBtn = document.querySelector('.editBtn');
+const cancel = document.querySelector('.cancel');
+const remains = document.querySelector(".remaining-tasks");
+const selectDiv = document.querySelector('.select-box');
+const selectAllBox = document.querySelector('.select-all');
+const error = document.querySelector(".error");
+
 
